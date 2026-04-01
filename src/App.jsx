cae5f95511815dd3f76e0d7edb2730b2dd7c2e0b
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-04-01 09:44:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './Components/NavBar'
+
+function App() {
+  
+
+  return (
+    <>
+     <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function NavBar(){
+    return(
+        <div className="w-full border h-20 ">
+            <h1>hello</h1>
+        </div>
+    )
+}

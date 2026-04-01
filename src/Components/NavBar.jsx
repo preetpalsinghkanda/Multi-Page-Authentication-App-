c5af5dafc7eddb00ed1fafd6f87cba1a443a753e
preetpalsinghkanda <preetpalsinghkanda@gmail.com>
2026-04-01 10:35:42 +0530

    setup tailwind (first time) and working on NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import logo from '../../public/download.jpg'
 
 
 export default function NavBar(){
     return(
-        <div className="w-full border h-20 ">
-            <h1>hello</h1>
+        <div className="w-full border h-20 flex items-center justify-evenly">
+
+
+            <div className="flex items-center"><img className="h-14" src={logo} alt="" /> <span>AuthApp</span></div>
+
+            <div className="flex gap-6">
+                <button className="">Home</button>
+                <button className="">About</button>
+            </div>
+            
+           <div className="flex gap-4">
+            <button className="border rounded px-4 py-2">login</button>
+            <button className="border rounded px-4 py-2">Sign Up</button>
+           </div>
         </div>
     )
 }
